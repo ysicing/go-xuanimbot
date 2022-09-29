99bebed1da665dd0440bd845bf8cdfef89d54560
ysicing <i@ysicing.me>
2022-09-29 16:56:30 +0800

    feat(update): update
    update
    
    Signed-off-by: ysicing <i@ysicing.me>

--- example/example.go
+++ example/example.go
@@ -1,34 +1,130 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"os"
 
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
 	xuanim "github.com/ysicing/go-xuanimbot"
 )
 
+func init() {
+	logrus.SetFormatter(&logrus.TextFormatter{})
+	logrus.SetOutput(os.Stdout)
+	logrus.SetLevel(logrus.InfoLevel)
+}
+
 func main() {
-	im, err := xuanim.New("token", "ysicing",
-		xuanim.WithBaseURL("http://127.0.0.1"),
-		xuanim.WithDevMode(),
-		xuanim.WithDumpAll(),
-	)
-	if err != nil {
-		log.Fatal(err)
+	app := cli.NewApp()
+	app.Name = "小喧喧大助手"
+	app.Usage = "小喧喧大助手"
+	app.Action = run
+	app.Version = "0.0.1"
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:   "debug",
+			Usage:  "debug",
+			EnvVar: "XIM_DEBUG",
+		},
+		cli.BoolFlag{
+			Name:   "all",
+			Usage:  "group all",
+			EnvVar: "XIM_GROUPALL",
+		},
+		cli.StringFlag{
+			Name:   "token",
+			Usage:  "token",
+			EnvVar: "XIM_TOKEN",
+		},
+		cli.StringFlag{
+			Name:   "caller",
+			Usage:  "caller",
+			EnvVar: "XIM_CALLER",
+		},
+		cli.StringFlag{
+			Name:   "api",
+			Usage:  "api",
+			EnvVar: "XIM_API",
+		},
+		cli.StringSliceFlag{
+			Name:   "users",
+			Usage:  "users",
+			EnvVar: "XIM_USERS",
+		},
+		cli.StringFlag{
+			Name:   "group",
+			Usage:  "group id",
+			EnvVar: "XIM_GID",
+		},
+		cli.StringFlag{
+			Name:   "title",
+			Usage:  "title",
+			EnvVar: "XIM_TITLE",
+		},
+		cli.StringFlag{
+			Name:   "message",
+			Usage:  "message",
+			EnvVar: "XIM_MESSAGE",
+		},
+		cli.StringFlag{
+			Name:   "url",
+			Usage:  "url",
+			EnvVar: "XIM_URL",
+		},
 	}
-	if _, _, err = im.Notification.SendUser(xuanim.UserMessage{
-		Users: []string{"ysicing"},
+	if err := app.Run(os.Args); err != nil {
+		logrus.Fatal(err)
+	}
+}
+
+func run(c *cli.Context) error {
+	var op []xuanim.ClientOptionFunc
+	op = append(op, xuanim.WithBaseURL(c.String("api")))
+	if c.Bool("debug") {
+		op = append(op, xuanim.WithDevMode(), xuanim.WithDumpAll())
+	}
+	im, err := xuanim.New(c.String("token"), c.String("caller"), op...)
+	if err != nil {
+		return err
+	}
+	users := c.StringSlice("users")
+	gid := c.String("group")
+	title := c.String("title")
+	if len(title) == 0 {
+		title = "收到一条消息"
+	}
+	msg := c.String("message")
+	if len(msg) == 0 {
+		msg = "测试嘿嘿"
+	}
+	if len(users) == 0 && len(gid) == 0 {
+		return fmt.Errorf("群组或者个人")
+	}
+	if len(users) > 0 {
+		if _, _, err = im.Notification.SendUser(xuanim.UserMessage{
+			Users: users,
+			MessageBody: xuanim.MessageBody{
+				Title:   title,
+				Content: msg,
+				URL:     c.String("url"),
+			},
+		}); err != nil {
+			return err
+		}
+	}
+	if c.Bool("all") {
+		msg = fmt.Sprintf("%s @all", msg)
+	}
+	if _, _, err = im.Notification.SendChat(xuanim.ChatMessage{
+		GID: gid,
 		MessageBody: xuanim.MessageBody{
-			Title: "66666",
+			Title:   title,
+			Content: msg,
+			URL:     c.String("url"),
 		},
 	}); err != nil {
-		log.Println(err)
+		return err
 	}
-	// if _, _, err = im.Notification.SendChat(xuanim.ChatMessage{
-	// 	GID: "8c",
-	// 	MessageBody: xuanim.MessageBody{
-	// 		Title: "66666",
-	// 	},
-	// }); err != nil {
-	// 	log.Println(err)
-	// }
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module github.com/ysicing/go-xuanimbot
 
 go 1.19
 
-require github.com/imroc/req/v3 v3.23.0
+require (
+	github.com/imroc/req/v3 v3.23.0
+	github.com/sirupsen/logrus v1.9.0
+	github.com/urfave/cli v1.22.10
+)
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
 	github.com/golang/mock v1.6.0 // indirect
@@ -18,6 +23,7 @@ require (
 	github.com/marten-seemann/qtls-go1-19 v0.1.0 // indirect
 	github.com/nxadm/tail v1.4.8 // indirect
 	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	golang.org/x/crypto v0.0.0-20220919173607-35f4265a4bc0 // indirect
 	golang.org/x/exp v0.0.0-20220916125017-b168a2c6b86b // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
