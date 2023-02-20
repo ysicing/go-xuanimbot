aab4974aead8081cd9a6b2a8d7b40be3800abdb0
ysicing <i@ysicing.me>
2023-02-20 11:07:17 +0800

    docs(readme): add example readme
    add example readme
    
    Signed-off-by: ysicing <i@ysicing.me>

--- example/example.go
+++ example/example.go
@@ -7,17 +7,11 @@ import xuanim "github.com/ysicing/go-xuanimbot"
 
 func main() {
 	customClient, _ := xuanim.New("123", "xxx", xuanim.WithBaseURL("https://demo.xuanim"), xuanim.WithCustom(true))
-	SendBody := xuanim.SendBody{
-		Title:       "",
-		Subtitle:    "",
-		Content:     "",
-		ContentType: "",
-		URL:         "",
-		Actions:     nil,
-		Sender:      xuanim.Sender{},
-	}
 	customClient.Notification.SendUser(xuanim.SendUserNotification{
 		Users: []string{"ysicing"},
-		SendBody,
+		SendBody: xuanim.SendBody{
+			Title:       "测试",
+			ContentType: xuanim.ContentTypeText,
+		},
 	})
 }
